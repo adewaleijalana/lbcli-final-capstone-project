871135e613d3008cc259a238f9d9792b0eabc178
adewaleijalana <adewaleijalana@gmail.com>
2025-04-22 21:44:56 +0100

    getting the number of outputs created by block 243825

--- submission/02.sh
+++ submission/02.sh
@@ -1,3 +1,3 @@
 # How many new outputs were created by block 243,825?
 
-bitcoin-cli -signet -rpcconnect=127.0.0.1 getblockstats 243825 | jq -r '.outs'
+bitcoin-cli -signet getblockstats 243825 | jq -r '.outs'
